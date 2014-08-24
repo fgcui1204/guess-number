bbd7a4ee0e9cc76f23fc7aca1df5e9129f9f8160
fgcui <fgcui1204@163.com>
2014-08-24 20:19:38 +0800

    four temps

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -7,15 +7,41 @@ describe('game_spec', function () {
     var compare = new CompareNumber();
     var guess=new Guess(answerGen,compare);
     beforeEach(function() {
-         input=['1234','2345','5678'];
-         spyOn(AnswerGenerrator,'isNumbers').and.returnValue('1234');
+         input=['1234','2245','56780','1a21'];
+         spyOn(answerGen,'isNumbers').and.returnValue('1234');
     });
 
     it('game is start', function () {
         var game = new Game(guess);
         var welcomeString = game.start();
-        expect(answerGen.isNumbers().calls.count()).toEqual(1);
+        //expect(answerGen.isNumbers.calls.count()).toEqual(1);
         expect(welcomeString).toEqual('welcome to you');
         expect(game.number).toEqual(6);
+    });
+    it('input is not repeat',function(){
+        var game = new Game(guess);
+        var welcomeString = game.start();
+        expect(game.judge(input[0])).toEqual(true);
+        expect(game.judge(input[1])).toEqual(false);
+    });
+    it('input is number',function(){
+        var game = new Game(guess);
+        var welcomeString = game.start();
+        expect(game.judgeNumber(input[0])).toEqual(true);
+        expect(game.judgeNumber(input[3])).toEqual(false);
+    });
+    it('the length of input is 4',function(){
+        var game = new Game(guess);
+        var welcomeString = game.start();
+        expect(game.judgeNumber(input[0])).toEqual(true);
+        expect(game.judgeNumber(input[2])).toEqual(false);
+    });
+    it('input is equal to answer', function(){
+
+        var game = new Game(guess);
+        var welcomeString = game.start();
+        var answer = game.judgeAnswer(input[0]);
+        expect(answer).toEqual('Congratulations');
+
     });
 });
--- src/game.js
+++ src/game.js
@@ -1,14 +1,41 @@
 /**
  * Created by fgcui on 14-8-22.
  */
-function Game(guess){
+function Game(guess) {
     this.guess = guess;
     this.answer;
     this.number;
 }
-Game.prototype.start=function(){
-    this.answer = this.guess.generator;
-    this.number=6;
-    var str="welcome to you";
+Game.prototype.start = function () {
+    this.answer = this.guess.generator.isNumbers();
+    this.number = 6;
+    var str = "welcome to you";
     return str;
+}
+Game.prototype.judge = function (input) {
+    if(input.length != 4){
+        return false;
+    }
+    for (var i = 0; i < input.length; i++) {
+        var cha = input.charAt(i);
+        var sub = input.substring(0,i)+input.substring(i+1);
+        if (sub.indexOf(cha)!=-1) {
+            console.log("有重复");
+            return false;
+        };
+    };
+    return true;
+}
+Game.prototype.judgeNumber = function (input){
+    var isNumber=/^\d{4}$/;
+    return isNumber.test(input);
+}
+Game.prototype.judgeAnswer = function (input){
+    var flag=this.guess.guessnumber(input);
+    console.log(this.answer);
+    if(this.answer == input){
+        return "Congratulations";
+    }else{
+        return flag;
+    }
 }
